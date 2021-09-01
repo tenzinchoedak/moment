1f24c239f26060b9af9a3237b7b6b1ee88c814fc
tenzin <tenzin@nine.ch>
2021-09-01 11:22:40 +0200

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #returns the fill title on a per-page basis
+    def full_title(page_title = '')
+        base_title = "Moment"
+        if page_title.empty?
+            base_title
+        else
+            page_title + " | " + base_title
+        end
+    end
 end
