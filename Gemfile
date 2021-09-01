4f0efe535ba6611018d74e1b92dd62dcbd4543e7
tenzin <tenzin@nine.ch>
2021-09-01 13:28:40 +0200

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.0'
 
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
+gem 'bootstrap-sass', '3.4.1'
 gem 'puma', '~> 5.0'
 gem 'sass-rails', '>= 6'
 gem 'webpacker', git: 'https://github.com/rails/webpacker.git'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,23 +2,23 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get static_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
   test "should get moment" do
-    get static_pages_moment_url
+    get moment_path
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_path
     assert_response :success
   end
 end
